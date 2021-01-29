0d5db440e17dae5736cdc702f690a7ccedca0a41
Benjamin Huelsen <benjamin.huelsen@dfki.de>
2021-01-29 19:38:08 +0100

    Added first skeleton of motor driver.

---
+++ include/motor_driver/MotorDriver.hpp
@@ -0,0 +1,34 @@
+#ifndef MOTOR_DRIVER_HPP
+#define MOTOR_DRIVER_HPP
+
+#include <stdint.h>
+
+namespace motor_driver
+{
+    struct motorState{
+        uint16_t position;
+        uint16_t velocity;
+        uint16_t current;
+    };
+
+    class MotorDriver{
+
+    public:
+        MotorDriver();
+
+	~MotorDriver();
+
+	motorState disableMotor();
+	motorState enableMotor();
+	motorState setZeroPosition();
+	motorState sendDegreeCommand();
+        motorState sendRadCommand();
+	motorState sendTorqueCommand();
+
+    private:
+
+	bool isEnabled;
+    };
+
+}
+#endif // MOTOR_DRIVER_HPP
---
+++ src/MotorDriver.cpp
@@ -0,0 +1,44 @@
+#include "motor_driver/MotorDriver.hpp"
+
+namespace motor_driver
+{
+
+MotorDriver::MotorDriver()
+{
+}
+
+MotorDriver::~MotorDriver()
+{
+}
+
+motorState MotorDriver::disableMotor()
+{
+motorState state;
+return state;
+}
+
+motorState MotorDriver::setZeroPosition()
+{
+motorState state;
+return state;
+}
+
+motorState MotorDriver::sendDegreeCommand()
+{
+motorState state;
+return state;
+}
+
+motorState MotorDriver::sendRadCommand()
+{
+motorState state;
+return state;
+}
+
+motorState MotorDriver::sendTorqueCommand()
+{
+motorState state;
+return state;
+}
+
+} // motor driver namespace
