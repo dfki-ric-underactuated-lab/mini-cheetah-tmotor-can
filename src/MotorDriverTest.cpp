54eba75f13050739ab0af0109566c738104cae76
Shubham Vyas <Shubham.Vyas@dfki.de>
2021-11-20 17:48:33 +0100

    latest 2 motor test on hopping leg

--- src/MotorDriverTest.cpp
+++ src/MotorDriverTest.cpp
@@ -24,20 +24,23 @@ uint64_t get_time_in_microseconds()
 
 int main(int argc, char **argv)
 {
-    vector<int> motor_ids = {0x01};
+    vector<int> motor_ids = {0x06, 0x07};
 	motor_driver::MotorDriver motor_controller(motor_ids, "can0", motor_driver::MotorType::AK80_6_V2);
 	
 	cout<<"Enabling Motor..."<<endl;
 	auto start_state = motor_controller.enableMotor(motor_ids);
 	cout<<"Position: "<<start_state[motor_ids[0]].position<<" Velocity: "<<start_state[motor_ids[0]].velocity<<" Torque: "<<start_state[motor_ids[0]].torque<<endl;
+	cout<<"Position: "<<start_state[motor_ids[1]].position<<" Velocity: "<<start_state[motor_ids[1]].velocity<<" Torque: "<<start_state[motor_ids[1]].torque<<endl;
 	
     cout<<"Setting Zero Position..."<<endl;
     auto stateZero = motor_controller.setZeroPosition(motor_ids);
     cout<<"Position: "<<stateZero[motor_ids[0]].position<<" Velocity: "<<stateZero[motor_ids[0]].velocity<<" Torque: "<<stateZero[motor_ids[0]].torque<<endl; 
+	cout<<"Position: "<<stateZero[motor_ids[1]].position<<" Velocity: "<<stateZero[motor_ids[1]].velocity<<" Torque: "<<stateZero[motor_ids[1]].torque<<endl; 
 	
 	cout<<"Disabling Motor..."<<endl;	
 	auto end_state = motor_controller.disableMotor(motor_ids);
 	cout<<"Position: "<<end_state[motor_ids[0]].position<<" Velocity: "<<end_state[motor_ids[0]].velocity<<" Torque: "<<end_state[motor_ids[0]].torque<<endl;
+	cout<<"Position: "<<end_state[motor_ids[1]].position<<" Velocity: "<<end_state[motor_ids[1]].velocity<<" Torque: "<<end_state[motor_ids[1]].torque<<endl;
 	
     return 0;
 }
